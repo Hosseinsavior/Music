95dcd9820745c3a86e70c76188efba3628a3f0e4
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2024-05-20 20:18:51 +0330

    Update bot.py

--- Music/plugins/bot.py
+++ Music/plugins/bot.py
@@ -1,5 +1,5 @@
 import datetime
-
+import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardMarkup, Message
@@ -15,59 +15,58 @@ from Music.helpers.strings import TEXTS
 from Music.helpers.users import MusicUser
 from Music.utils.youtube import ytube
 
-
 @hellbot.app.on_message(filters.command(["start", "alive"]) & ~Config.BANNED_USERS)
 @check_mode
 async def start(_, message: Message):
+    if len(message.command) > 1:
+        deep_cmd = message.text.split(None, 1)[1]
+        if deep_cmd.startswith("song"):
+            results = await ytube.get_data(deep_cmd.split("_", 1)[1], True)
+            about = TEXTS.ABOUT_SONG.format(
+                results[0]["title"],
+                results[0]["channel"],
+                results[0]["published"],
+                results[0]["views"],
+                results[0]["duration"],
+                hellbot.app.mention,
+            )
+            await message.reply_photo(
+                results[0]["thumbnail"],
+                caption=about,
+                reply_markup=InlineKeyboardMarkup(
+                    Buttons.song_details_markup(
+                        results[0]["link"],
+                        results[0]["ch_link"],
+                    )
+                ),
+            )
+            return
+        elif deep_cmd.startswith("user"):
+            userid = int(deep_cmd.split("_", 1)[1])
+            userdbs = await db.get_user(userid)
+            songs = userdbs["songs_played"]
+            level = MusicUser.get_user_level(int(songs))
+            to_send = TEXTS.ABOUT_USER.format(
+                userdbs["user_name"],
+                userdbs["user_id"],
+                level,
+                songs,
+                userdbs["join_date"],
+                hellbot.app.mention,
+            )
+            await message.reply_text(
+                to_send,
+                reply_markup=InlineKeyboardMarkup(Buttons.close_markup()),
+                disable_web_page_preview=True,
+            )
+            return
+        elif deep_cmd.startswith("help"):
+            await message.reply_text(
+                TEXTS.HELP_PM.format(hellbot.app.mention),
+                reply_markup=InlineKeyboardMarkup(Buttons.help_pm_markup()),
+            )
+            return
     if message.chat.type == ChatType.PRIVATE:
-        if len(message.command) > 1:
-            deep_cmd = message.text.split(None, 1)[1]
-            if deep_cmd.startswith("song"):
-                results = await ytube.get_data(deep_cmd.split("_", 1)[1], True)
-                about = TEXTS.ABOUT_SONG.format(
-                    results[0]["title"],
-                    results[0]["channel"],
-                    results[0]["published"],
-                    results[0]["views"],
-                    results[0]["duration"],
-                    hellbot.app.mention,
-                )
-                await message.reply_photo(
-                    results[0]["thumbnail"],
-                    caption=about,
-                    reply_markup=InlineKeyboardMarkup(
-                        Buttons.song_details_markup(
-                            results[0]["link"],
-                            results[0]["ch_link"],
-                        )
-                    ),
-                )
-                return
-            elif deep_cmd.startswith("user"):
-                userid = int(deep_cmd.split("_", 1)[1])
-                userdbs = await db.get_user(userid)
-                songs = userdbs["songs_played"]
-                level = MusicUser.get_user_level(int(songs))
-                to_send = TEXTS.ABOUT_USER.format(
-                    userdbs["user_name"],
-                    userdbs["user_id"],
-                    level,
-                    songs,
-                    userdbs["join_date"],
-                    hellbot.app.mention,
-                )
-                await message.reply_text(
-                    to_send,
-                    reply_markup=InlineKeyboardMarkup(Buttons.close_markup()),
-                    disable_web_page_preview=True,
-                )
-                return
-            elif deep_cmd.startswith("help"):
-                await message.reply_text(
-                    TEXTS.HELP_PM.format(hellbot.app.mention),
-                    reply_markup=InlineKeyboardMarkup(Buttons.help_pm_markup()),
-                )
-                return
         await message.reply_text(
             TEXTS.START_PM.format(
                 message.from_user.first_name,
@@ -79,6 +78,7 @@ async def start(_, message: Message):
         )
     elif message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
         await message.reply_text(TEXTS.START_GC)
+
 
 
 @hellbot.app.on_message(filters.command("help") & ~Config.BANNED_USERS)
