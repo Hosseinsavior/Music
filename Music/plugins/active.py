d5b11f0c6627f71175952f3a145f45af5fff1b80
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2024-05-21 15:49:54 +0330

    Update active.py

--- Music/plugins/active.py
+++ Music/plugins/active.py
@@ -14,10 +14,7 @@ from Music.utils.pages import MakePages
 from Music.utils.queue import Queue
 
 
-@hellbot.app.on_message(filters.command("active") & Config.SUDO_USERS)
-@check_mode
-async def activevc(_, message: Message):
-    hell = await message.reply_text(f"Getting active voice chats ...")
+async def get_active_chats():
     active_chats = await db.get_active_vc()
     collection = []
     for x in active_chats:
@@ -50,7 +47,15 @@ async def activevc(_, message: Message):
             "vc_type": vc_type,
         }
         collection.append(context)
-    if len(collection) == 0:
+    return collection
+
+
+@hellbot.app.on_message(filters.command("active") & Config.SUDO_USERS)
+@check_mode
+async def activevc(_, message: Message):
+    hell = await message.reply_text(f"Getting active voice chats ...")
+    collection = await get_active_chats()
+    if not collection:
         return await hell.edit(f"No active voice chats found!")
     await MakePages.activevc_page(hell, collection, 0, 0, True)
 
@@ -60,38 +65,9 @@ async def activevc_cb(_, cb: CallbackQuery):
     data = cb.data.split("|")
     cmd = data[1]
     page = int(data[2])
-    collection = []
-    active_chats = await db.get_active_vc()
-    for x in active_chats:
-        cid = int(x["chat_id"])
-        if cid == 0:
-            continue
-        joined = x["join_time"]
-        vc_type = x["vc_type"]
-        participants = len(await hellmusic.vc_participants(cid))
-        try:
-            check = Queue.get_queue(cid)
-            song = check[0]["title"]
-        except Exception as e:
-            LOGS.error(e)
-            song = "Unknown"
-        try:
-            title = (await hellbot.app.get_chat(cid)).title
-        except Exception:
-            title = "Private Group"
-        active_since = datetime.datetime.now() - joined
-        total_secs = active_since.total_seconds()
-        _hours = int(total_secs // 3600)
-        _minutes = int(total_secs % 3600 // 60)
-        context = {
-            "chat_id": cid,
-            "title": title,
-            "participants": participants - 1,
-            "active_since": f"{_hours} hrs, {_minutes} mins.",
-            "playing": song,
-            "vc_type": vc_type,
-        }
-        collection.append(context)
+    collection = await get_active_chats()
+    if not collection:
+        return await cb.message.edit_text(f"No active voice chats found!")
     last_page, _ = formatter.group_the_list(collection, length=True)
     last_page -= 1
     if page == 0 and cmd == "prev":
